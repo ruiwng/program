a572194f02ab0dc49e00d94cd3754d0f6a810469
ruiwng <ruiwng@zju.edu.cn>
2014-12-18 14:22:30 +0800

    u Calculate e

---
+++ ZOJ/1113.cpp
@@ -0,0 +1,37 @@
+/*#include  <stdio.h>
+
+int main()
+{
+    printf("n e\n");
+    printf("- -----------\n");
+    double temp=1.0;
+    double result=0.0;
+    for(int i=0;i<=9;++i)
+    {
+        if(i!=0)
+            temp/=i;
+        result+=temp;
+        printf("%d %.10g\n",i,result);
+    }
+    return 0;
+}
+*/
+
+#include  <stdio.h>
+
+int main()
+{
+    printf("n e\n");
+    printf("- -----------\n");
+    printf("0 1\n");
+    printf("1 2\n");
+    printf("2 2.5\n");
+    printf("3 2.666666667\n");
+    printf("4 2.708333333\n");
+    printf("5 2.716666667\n");
+    printf("6 2.718055556\n");
+    printf("7 2.718253968\n");
+    printf("8 2.718278770\n");
+    printf("9 2.718281527\n");
+    return 0;
+}
