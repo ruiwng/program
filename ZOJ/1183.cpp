072a734d22ebe9868ce24e49d44d36573beba0e1
ruiwng <ruiwng@zju.edu.cn>
2014-12-24 15:32:11 +0800

    Scheduling Lectures

--- ZOJ/1183.cpp
+++ ZOJ/1183.cpp
@@ -1,84 +1,3 @@
-/*#include  <stdio.h>
-#include  <vector>
-using namespace std;
-
-int L,C;
-bool flag;
-int mini_number;
-int mini_dissatis;
-int n;
-vector<int> topic_time;
-
-int satis_func(int t)
-{
-    if(t==0)
-        return 0;
-    else if(t>=1&&t<=10)
-        return -C;
-    else
-        return (t-10)*(t-10);
-}
-
-void mini_func(int number,int index,int distasfy)
-{
-    if(index==n)
-    {
-        if(flag==false||number<mini_number||(number==mini_number&&distasfy<mini_dissatis))
-        {
-            mini_number=number;
-            mini_dissatis=distasfy;
-            flag=true;
-        }
-        return;
-    }
-    int lecture_time=L;
-    while(index<n&&topic_time[index]<lecture_time)
-        lecture_time-=topic_time[index++];
-    if(index!=n&&topic_time[index]==lecture_time&&topic_time[index]>=1&&topic_time[index]<=10)
-    {
-        mini_func(number+1,index,distasfy-C);
-        mini_func(number+1,index+1,distasfy);
-    }
-    else
-    {
-        if(index!=n&&topic_time[index]==lecture_time)
-            lecture_time-=topic_time[index++];
-        mini_func(number+1,index,distasfy+satis_func(lecture_time));
-    }
-}
-int main()
-{
-    int N;
-    while(scanf("%d",&N)!=EOF)
-    {
-        for(int i=0;i<N;++i)
-        {
-            if(i!=0)
-                printf("\n");
-            int case_number=0;
-            while(scanf("%d",&n)!=EOF&&n)
-            {
-                scanf("%d%d",&L,&C);
-                topic_time.clear();
-                topic_time.reserve(n);
-                for(int j=0;j<n;++j)
-                {
-                    int t;
-                    scanf("%d",&t);
-                    topic_time.push_back(t);
-                }
-                flag=false;
-                mini_func(0,0,0);
-                printf("Case %d:\n\n",++case_number);
-                printf("Minimum number of lectures: %d\n",mini_number);
-                printf("Total dissatisfaction index: %d\n\n",mini_dissatis);
-            }
-        }
-    }
-    return 0;
-}
-*/
-
 #include  <stdio.h>
 #include  <string.h>
 #include  <vector>
@@ -99,7 +18,7 @@ int L,C;
 int n;
 vector<int> topic_time;
 
-int satis_func(int t)
+inline int satis_func(int t)
 {
     if(t==0)
         return 0;
@@ -133,9 +52,7 @@ pair<int,int> mini_func(int index)
         --index;
         lecture_time+=topic_time[index];
     }while(index>start);
-    record_array[index].tag=1;
-    record_array[index].number=number;
-    record_array[index].dissatisfy=dissatisfy;
+    record_array[index]=record(1,number,dissatisfy);
     return make_pair(number,dissatisfy);
 }
 
