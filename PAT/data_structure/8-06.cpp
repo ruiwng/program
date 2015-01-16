6c29b45c80dfb472c2bfd0c8cceafa362ca524f0
ruiwng <ruiwng@zju.edu.cn>
2015-01-16 19:14:34 +0800

    Minimum Cost

--- PAT/data_structure/8-06.cpp
+++ PAT/data_structure/8-06.cpp
@@ -1,23 +1,15 @@
 #include  <stdio.h>
 #include  <vector>
 #include  <queue>
-
 using namespace std;
-
-unsigned int disjoint_find(vector<unsigned int>& disjoint,unsigned int element)
+vector<int> disjoint;
+int disjoint_find(int element)
 {
 	if(disjoint[element]==0)
 		return element;
 	else
-		return disjoint[element]=disjoint_find(disjoint,disjoint[element]);
+		return disjoint[element]=disjoint_find(disjoint[element]);
 }
-
-void disjoint_union(vector<unsigned int>& disjoint,unsigned int element1,unsigned int element2)
-{
-
-	disjoint[disjoint_find(disjoint,element2)]=element1;
-}
-
 struct cost_info
 {
 	unsigned int begin;
@@ -31,43 +23,44 @@ struct cost_info
 };
 int main()
 {
-	unsigned int number_village;
-	while(scanf("%u",&number_village)!=EOF)
+	int number_village;
+	while(scanf("%d",&number_village)!=EOF)
 	{
-		vector<unsigned int> disjoint_set(number_village+1);
-		for(unsigned int i=1;i<=number_village;++i)
-			disjoint_set[i]=0;
-
-		unsigned int temp=number_village*(number_village-1)/2;
+		disjoint.assign(number_village+1,0);
+	    int temp=number_village*(number_village-1)/2;
 		priority_queue<cost_info> unconstructed_array;
-		unsigned int number_sets=number_village;
-		for(unsigned int i=0;i<temp;++i)
+		int number_sets=number_village;
+		for(int i=0;i<temp;++i)
 		{
-			unsigned int village1,village2;
-			unsigned int cost;
-			unsigned int state;
-			scanf("%u %u %u %u",&village1,&village2,&cost,&state);
-			if(state==1&&disjoint_find(disjoint_set,village1)!=disjoint_find(disjoint_set,village2))
+			int village1,village2;
+			int cost;
+			int state;
+			scanf("%d %d %d %d",&village1,&village2,&cost,&state);
+			int parent1=disjoint_find(village1);
+			int parent2=disjoint_find(village2);
+			if(state==1&&parent1!=parent2)
 			{
-				disjoint_union(disjoint_set,village1,village2);
+			   disjoint[parent2]=parent1;
 			   --number_sets;
 			}
 			else if(state==0)
                 unconstructed_array.push(cost_info(village1,village2,cost));
 		}
-		unsigned int cost_sum=0;
+		int cost_sum=0;
 		while(number_sets!=1)
 		{
 			const cost_info& min_cost=unconstructed_array.top();
-			if(disjoint_find(disjoint_set,min_cost.begin)!=disjoint_find(disjoint_set,min_cost.end))
+			int parent1=disjoint_find(min_cost.begin);
+			int parent2=disjoint_find(min_cost.end);
+			if(parent1!=parent2)
 			{
 				cost_sum+=min_cost.cost;
-				disjoint_union(disjoint_set,min_cost.begin,min_cost.end);
+				disjoint[parent2]=parent1;
 				--number_sets;
 			}
 			unconstructed_array.pop();
 		}
-		printf("%u\n",cost_sum);
+		printf("%d\n",cost_sum);
 	}
 	return 0;
 }
