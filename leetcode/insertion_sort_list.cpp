96e769c5dbf002539deb9fe1feae6d4cac1b2928
ruiwng <ruiwng@zju.edu.cn>
2015-02-09 15:09:35 +0800

    Insertion Sort List

--- leetcode/insertion_sort_list.cpp
+++ leetcode/insertion_sort_list.cpp
@@ -1,13 +1,24 @@
 #include  <stdio.h>
+#include  <algorithm>
+#include  <vector>
+using namespace std;
 
 struct ListNode {
      int val;
      ListNode *next;
      ListNode(int x) : val(x), next(NULL) {}
  };
- 
+
+struct cmp_func
+{
+    bool operator()(ListNode *lhs,ListNode *rhs) const
+    {
+        return lhs->val<rhs->val;
+    }
+};
 class Solution {
 public:
+/*
     ListNode *insertionSortList(ListNode *head) {
         if(head==NULL)
             return head;
@@ -32,6 +43,24 @@ public:
             p=q;
         }
         return head;
+    }*/
+    ListNode *insertionSortList(ListNode *head){
+        vector<ListNode*> node_array;
+        ListNode *p=head;
+        while(p!=NULL)
+        {
+            node_array.push_back(p);
+            p=p->next;
+        }
+        sort(node_array.begin(),node_array.end(),cmp_func());
+        node_array.push_back(NULL);
+        int k=0;
+        while(node_array[k]!=NULL)
+        {
+            node_array[k]->next=node_array[k+1];
+            ++k;
+        }
+        return node_array[0];
     }
 };
 
