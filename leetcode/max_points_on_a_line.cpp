ba1006e82d31b34f4ff0357dea388c14456f3631
ruiwng <ruiwng@zju.edu.cn>
2015-02-10 21:46:27 +0800

    Max Points On A Line

--- leetcode/max_points_on_a_line.cpp
+++ leetcode/max_points_on_a_line.cpp
@@ -25,43 +25,42 @@ public:
         int len=points.size();
 		if(len==1)
 			return 1;
+		sort(points.begin(),points.end(),comp_func());
 		int max_points=0;
-        sort(points.begin(),points.end(),comp_func());
-        len=points.size();
-		for(int i=0;i<len-1;++i)
+		for(int i=0;i<len;++i)
 		{
-            int num1=0;
-            int current=1;
+			int current_max=1;
 			for(int j=i+1;j<len;++j)
 			{
-                if(points[j].x==points[i].x&&points[j].y==points[i].y)
+                if(points[j].x==points[i].x&&points[j].y==points[i].y)//duplication of the first point
                 {
-                    ++num1;
-                    continue;
+                    if(++current_max>max_points)
+						max_points=current_max;
+					i=j;
+					continue;
                 }
-                int num2=0;
-				int current_points=2;
+				int current_points=current_max+1;
 				int temp_x=points[j].x-points[i].x;
                 int temp_y=points[j].y-points[i].y;
 				for(int k=j+1;k<len;++k)
 				{
-                    if(points[k].x==points[j].x&&points[k].y==points[j].y)
-                    {
-                        ++num2;
-                        continue;
-                    }
+					if(points[k].x==points[j].x&&points[k].y==points[j].y)//duplication of the second point
+					{
+						++current_points;
+						j=k;
+						continue;
+					}
 				    if(temp_x*(points[k].y-points[i].y)==temp_y*(points[k].x-points[i].x))
                             ++current_points;
 				}
-				if(current_points+num1+num2>max_points)
-					max_points=current_points+num1+num2;
+				if(current_points>max_points)
+					max_points=current_points;
 			}
-            if(current+num1>max_points)
-                max_points=current+num1;
 		}
 		return max_points;
     }
 };
+
 int main()
 {
     vector<Point> point_array;
