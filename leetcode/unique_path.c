295bb34587a9327632064d0e8a5a345101d0db65
ruiwng <ruiwng@zju.edu.cn>
2015-03-15 16:00:47 +0800

    Unique Path

---
+++ leetcode/unique_path.c
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include  <string.h>
+
+int uniquePaths(int m, int n) {
+    int grid[100][100];
+	memset(grid,0,sizeof(grid));
+	for(int i=0;i<m;++i)
+		grid[i][0]=1;
+	for(int i=0;i<n;++i)
+		grid[0][i]=1;
+	for(int i=1;i<m;++i)
+		for(int j=1;j<n;++j)
+			grid[i][j]=grid[i-1][j]+grid[i][j-1];
+	return grid[m-1][n-1];
+}
+
+int main()
+{
+
+}
