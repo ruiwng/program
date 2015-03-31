1ca693bff7f28668a5764db6dbc6b0eb3b2278cb
ruiwng <ruiwng@zju.edu.cn>
2015-04-01 04:24:02 +0800

    Insertion Sort List

--- leetcode/insertion_sort_list.cpp
+++ leetcode/insertion_sort_list.cpp
@@ -1,70 +1,28 @@
-#include  <stdio.h>
-#include  <algorithm>
-#include  <vector>
-using namespace std;
+#include  <stdlib.h>
 
 struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode(int x) : val(x), next(NULL) {}
- };
+    int val;
+    struct ListNode *next;
+};
 
-struct cmp_func
-{
-    bool operator()(ListNode *lhs,ListNode *rhs) const
-    {
-        return lhs->val<rhs->val;
-    }
-};
-class Solution {
-public:
-/*
-    ListNode *insertionSortList(ListNode *head) {
-        if(head==NULL)
-            return head;
-        ListNode *p=head->next;
-        head->next=NULL;
-        while(p!=NULL)
-        {
-            ListNode *q=p->next;
-            if(p->val<=head->val)
-            {
-                p->next=head;
-                head=p;
-            }
-            else
-            {
-                ListNode *k=head;
-                while(k->next!=NULL&&p->val>k->next->val)
-                    k=k->next;
-                p->next=k->next;
-                k->next=p;
-            }
-            p=q;
-        }
-        return head;
-    }*/
-    ListNode *insertionSortList(ListNode *head){
-        vector<ListNode*> node_array;
-        ListNode *p=head;
-        while(p!=NULL)
-        {
-            node_array.push_back(p);
-            p=p->next;
-        }
-        sort(node_array.begin(),node_array.end(),cmp_func());
-        node_array.push_back(NULL);
-        int k=0;
-        while(node_array[k]!=NULL)
-        {
-            node_array[k]->next=node_array[k+1];
-            ++k;
-        }
-        return node_array[0];
-    }
-};
+struct ListNode *insertionSortList(struct ListNode *head) {
+    struct ListNode *h=(struct ListNode*)malloc(sizeof(struct ListNode));
+	h->next=NULL;
+	struct ListNode *q=head;
+	while(q!=NULL)
+	{
+		struct ListNode *t=q->next;
+		struct ListNode *p=h;
+		while(p->next!=NULL&&p->next->val<q->val)
+			p=p->next;
+		q->next=p->next;
+		p->next=q;
+		q=t;
+	}
+	return h->next;
+}
 
 int main()
 {
-    
+
 }
