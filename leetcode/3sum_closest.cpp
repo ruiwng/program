0335a12a429922b83f47951b81ff76b4f1675fa6
ruiwng <ruiwng@zju.edu.cn>
2015-04-09 21:31:00 +0800

    3 Sum Closest

--- leetcode/3sum_closest.cpp
+++ leetcode/3sum_closest.cpp
@@ -4,11 +4,11 @@ int threeSumClosest(int *num, int n, int target) {
      bool first=true;
         bool exist=false;
         int sum=0;
-        for(unsigned int i=0;i<n;++i)
+        for(int i=0;!exist&&i<n;++i)
         {
-          for(unsigned int j=i+1;j<n;++j)
+          for(int j=i+1;!exist&&j<n;++j)
           {
-             for(unsigned int k=j+1;k<n;++k)
+             for(int k=j+1;!exist&&k<n;++k)
              {
                     int temp_sum=num[i]+num[j]+num[k];
                     if(first||abs(temp_sum-target)<abs(sum-target))       
@@ -16,17 +16,10 @@ int threeSumClosest(int *num, int n, int target) {
                          first=false;
                          sum=temp_sum;
                          if(sum==target)
-                         {
                              exist=true;
-                             break;
-                         }
                     }
              }
-             if(exist)
-                break;
           }
-          if(exist)
-             break;
         }
         return sum;
 }
