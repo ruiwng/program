2117f46f2472f0a21974351f0a9c01b38933cb90
ruiwng <ruiwng@zju.edu.cn>
2015-04-09 19:53:21 +0800

    All Roads Lead to Rome

--- PAT/advanced/1087.cpp
+++ PAT/advanced/1087.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 #define    MAX_DISTANCE      1000000000
 
-unsigned int distance_info[201][201];
+int distance_info[201][201];
 
 struct node_info
 {
     string name;
-    unsigned int parent;
-    unsigned int distance;
-    unsigned int happiness;
-    unsigned int city_count;
-    unsigned int path_count;
+    int parent;
+    int distance;
+    int happiness;
+    int city_count;
+    int path_count;
     bool visit;
     node_info():name(),parent(0),distance(MAX_DISTANCE),happiness(0),city_count(0),path_count(0),visit(false){
     
     }
 };
 
-void print_path(vector<node_info>& info,unsigned int end)
+void print_path(vector<node_info>& info,int end)
 {
     if(end!=0)
     {
@@ -35,31 +35,31 @@ void print_path(vector<node_info>& info,unsigned int end)
 
 int main()
 {
-    unsigned int N,K;
+    int N,K;
     char start[4];
-    while(scanf("%u %u %s",&N,&K,start)!=EOF)
+    while(scanf("%d %d %s",&N,&K,start)!=EOF)
     {
-        memset(distance_info,'\0',sizeof(distance_info));
-        unordered_map<string,unsigned int> index_info;
+        memset(distance_info,0,sizeof(distance_info));
+        unordered_map<string,int> index_info;
         index_info[start]=0;
-        vector<unsigned int> happiness(N);
+        vector<int> happiness(N);
         vector<node_info> node_array(N);
         node_array[0].name=start;
-        unsigned int dest;
-        for(unsigned int i=1;i<N;++i)
+        int dest;
+        for(int i=1;i<N;++i)
         {
             char pos[4];
-            scanf("%s %u",pos,&happiness[i]);
+            scanf("%s %d",pos,&happiness[i]);
             index_info[pos]=i;
             node_array[i].name=pos;
             if(strcmp(pos,"ROM")==0)
                 dest=i;
         }
-        for(unsigned int i=0;i<K;++i)
+        for(int i=0;i<K;++i)
         {
             char begin[4],end[4];
-            unsigned int dist;
-            scanf("%s %s %u",begin,end,&dist);
+            int dist;
+            scanf("%s %s %d",begin,end,&dist);
             distance_info[index_info[begin]][index_info[end]]=dist;
             distance_info[index_info[end]][index_info[begin]]=dist;
             
@@ -68,9 +68,9 @@ int main()
         node_array[0].path_count=1;
         while(true)
         {
-            unsigned int min_distance=MAX_DISTANCE;
-            unsigned int current_node;
-            for(unsigned int i=0;i<N;++i)
+            int min_distance=MAX_DISTANCE;
+            int current_node;
+            for(int i=0;i<N;++i)
             {
                 if(node_array[i].visit)
                     continue;
@@ -83,7 +83,7 @@ int main()
             node_array[current_node].visit=true;
             if(current_node==dest)
                 break;
-            for(unsigned int i=0;i<N;++i)
+            for(int i=0;i<N;++i)
             {
                 if(node_array[i].visit||distance_info[current_node][i]==0)
                     continue;
@@ -109,7 +109,7 @@ int main()
                 }              
             }
         }
-        printf("%u %u %u %u\n",node_array[dest].path_count,node_array[dest].distance,
+        printf("%d %d %d %d\n",node_array[dest].path_count,node_array[dest].distance,
                node_array[dest].happiness,node_array[dest].happiness/node_array[dest].city_count);
         print_path(node_array,dest);
         printf("\n");
