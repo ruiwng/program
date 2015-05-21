119438c9e788bb97cb3985c6a9d756324e1b5eca
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 16:35:44 +0800

    个位数统计

--- PAT/basic/1021.cpp
+++ PAT/basic/1021.cpp
@@ -1,20 +1,16 @@
 #include  <stdio.h>
-#include  <stdlib.h>
 #include  <string.h>
-
 int main()
 {
 	char ch[1001];
 	int num[10];
 	while(scanf("%s",ch)!=EOF)
 	{
-		 memset(num,'\0',10*sizeof(int));
-	     int len=strlen(ch);
-		 for(int i=0;i<len;i++)
-		 {
-			 num[ch[i]-'0']++;
-		 }
-		 for(int i=0;i<10;i++)
+		 memset(num,0,sizeof(num));
+		 char *p=ch;
+		 while(*p!='\0')
+		   ++num[*p++-'0'];
+		 for(int i=0;i<10;++i)
 		 {
 			 if(num[i]!=0)
 			   printf("%d:%d\n",i,num[i]);
