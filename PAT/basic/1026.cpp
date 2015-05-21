fde1f3dfddb7390d6f3a330e6bdea7e27c39f8ed
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 16:38:21 +0800

    程序运行时间

--- PAT/basic/1026.cpp
+++ PAT/basic/1026.cpp
@@ -2,17 +2,17 @@
 
 int main()
 {
-    unsigned int start,end;
-    while(scanf("%u %u",&start,&end)!=EOF)
+    int start,end;
+    while(scanf("%d %d",&start,&end)!=EOF)
     {
-        unsigned int length=(end-start)/100;
-        unsigned int ss=length%60;
+        int length=(end-start)/100;
+        int ss=length%60;
         if((end-start)%100>=50)
             ++ss;
-        unsigned int temp=length/60;
-        unsigned int mm=temp%60;
+        int temp=length/60;
+        int mm=temp%60;
         temp/=60;
-        printf("%02u:%02u:%02u\n",temp,mm,ss);
+        printf("%02d:%02d:%02d\n",temp,mm,ss);
     }
     return 0;
 }
