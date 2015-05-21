6a3dcabd37fb8236807562c35d5c2141b590da87
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 15:59:48 +0800

    数素数

--- PAT/basic/1013.cpp
+++ PAT/basic/1013.cpp
@@ -1,43 +1,44 @@
 #include  <stdio.h>
-#include  <stdlib.h>
 #include  <string.h>
 
-int b[200000];
+#define MAX_LENGTH   105000
+
+char tag[MAX_LENGTH];
+int primes[10000];
+
 int main()
 {
-	memset(b,'\0',200000*sizeof(int));
-	for(int i=2;i<100000;i++)
+	memset(tag,0,sizeof(tag));
+	int index=0;
+	for(int i=2;i<MAX_LENGTH;++i)
 	{
-		if(b[i]==0)
+		if(tag[i]==0)
 		{
-			int j=200000/i;
-			for(int k=2;k<=j;k++)
-				b[k*i]=1;
+			primes[index++]=i;
+			if(index==10000)
+				break;
+			int k=i<<1;
+			for(;k<MAX_LENGTH;k+=i)
+				tag[k]=1;
 		}
 	}
 	int m,n;
-	while(scanf("%d %d",&m,&n)!=EOF)
+	while(scanf("%d%d",&m,&n)!=EOF)
 	{
-		int cnt1=0,cnt2=0;
-		for(int i=2;i<200000;i++)
+		int j=0;
+		for(int i=m;i<=n;++i)
 		{
-			if(b[i]==0)
+			printf("%d",primes[i-1]);
+			if(++j==10)
 			{
-				cnt1++;
-				if(cnt1>=m&&cnt1<=n)
-				{
-					cnt2++;
-					printf("%d",i);
-					if(cnt2%10!=0&&cnt1!=n)
-						printf(" ");
-					if(cnt2>=10&&cnt2%10==0&&cnt1<n)
-						printf("\n");
-				}
-				if(cnt1>n)
-					break;
+				printf("\n");
+				j=0;
 			}
+			else if(i!=n)
+				printf(" ");
 		}
-		printf("\n");
+		if(j!=0)
+			printf("\n");
 	}
 	return 0;
 }
