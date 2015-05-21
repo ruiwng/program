38d5eda480e8bed6cf5f0f8603d35d789f14dfd5
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 14:48:46 +0800

    A+B和C

--- PAT/basic/1011.cpp
+++ PAT/basic/1011.cpp
@@ -1,28 +1,18 @@
 #include  <stdio.h>
-#include  <stdlib.h>
-#include  <vector>
-using namespace std;
+
 int main()
 {
 	int n;
 	while(scanf("%d",&n)!=EOF)
 	{
-		vector<int> intVec;
-		for(int i=0;i<n;i++)
+		for(int i=1;i<=n;++i)
 		{
 			long long a,b,c;
 			scanf("%lld %lld %lld",&a,&b,&c);
 			if(a+b>c)
-				intVec.push_back(1);
+				printf("Case #%d: true\n",i);
 			else
-				intVec.push_back(0);
-		}
-		for(unsigned int i=0;i<intVec.size();++i)
-		{
-			if(intVec[i]==1)
-				printf("Case #%d: true\n",i+1);
-			else
-				printf("Case #%d: false\n",i+1);
+				printf("Case #%d: false\n",i);
 		}
 	}
 	return 0;
