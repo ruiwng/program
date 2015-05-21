47a1448c2da99437fa1c7e91381dc1262dc6c04f
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 16:34:27 +0800

    锤子剪刀布

--- PAT/basic/1018.cpp
+++ PAT/basic/1018.cpp
@@ -1,15 +1,12 @@
 #include  <stdio.h>
-#include  <map>
-#include  <vector>
-using namespace std;
 
 int arr[256][256];
 int win_arr[2][256];
 
 int main()
 {
-	unsigned int num;
-	while(scanf("%u\n",&num)!=EOF)
+	int num;
+	while(scanf("%d\n",&num)!=EOF)
 	{
 		arr['C']['C']=0;
 	    arr['C']['J']=1;
@@ -22,9 +19,9 @@ int main()
 	    arr['B']['C']=1;
 	    arr['B']['J']=-1;
 	    arr['B']['B']=0;
-
-		vector<int> info(3);
-		for(unsigned int i=0;i<num;++i)
+            int info[3];
+	    info[0]=info[1]=info[2]=0;
+		for(int i=0;i<num;++i)
 		{
 			char temp1,temp2;
 			char str[5];
@@ -35,21 +32,21 @@ int main()
 			switch(result)
 			{
 			case 1:
-				info[0]++;
-				win_arr[0][temp1]++;
+				++info[0];
+				++win_arr[0][temp1];
 				break;
 			case 0:
-				info[1]++;
+				++info[1];
 				break;
 			case -1:
-				info[2]++;
-				win_arr[1][temp2]++;
+				++info[2];
+				++win_arr[1][temp2];
 				break;
 			}
 		}
 		printf("%d %d %d\n",info[0],info[1],info[2]);
 		printf("%d %d %d\n",info[2],info[1],info[0]);
-		for(unsigned int i=0;i<2;++i)
+		for(int i=0;i<2;++i)
 		{
 			if(win_arr[i]['B']>=win_arr[i]['C']&&win_arr[i]['B']>=win_arr[i]['J'])
 				printf("B");
