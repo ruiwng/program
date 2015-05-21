db7b7c736aedb899e852e148617b3db26c3b87c7
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 16:31:26 +0800

    人口普查

--- PAT/basic/1028.cpp
+++ PAT/basic/1028.cpp
@@ -3,10 +3,10 @@
 
 struct birthday
 {
-    unsigned int year;
-    unsigned int month;
-    unsigned int day;
-    birthday(unsigned int y=0,unsigned int m=0,unsigned int d=0):year(y),month(m),day(d){
+    int year;
+    int month;
+    int day;
+    birthday(int y=0,int m=0,int d=0):year(y),month(m),day(d){
     }
 
     bool operator<(const birthday& b) const
@@ -24,7 +24,7 @@ struct person
     char name[20];
     birthday birth;
     person():birth(){
-        memset(name,'\0',sizeof(name));
+        memset(name,0,sizeof(name));
     }
     bool operator<(const person& p) const
     {
@@ -34,19 +34,19 @@ struct person
 
 int main()
 {
-    unsigned int number;
-    while(scanf("%u",&number)!=EOF)
+    int number;
+    while(scanf("%d",&number)!=EOF)
     {
         birthday temp1(2014,9,6);
         birthday temp2(1814,9,6);
         person person_temp;
-        unsigned int valid_count=0;
+        int valid_count=0;
         person old_person;
         person small_person;
         bool first=true;
-        for(unsigned int i=0;i<number;++i)
+        for(int i=0;i<number;++i)
         {
-            scanf("%s %u/%u/%u",person_temp.name,&person_temp.birth.year,
+            scanf("%s %d/%d/%d",person_temp.name,&person_temp.birth.year,
                   &person_temp.birth.month,&person_temp.birth.day);
             if(temp1<person_temp.birth||person_temp.birth<temp2)
                 continue;
