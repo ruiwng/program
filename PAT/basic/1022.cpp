d161a3127dc5a6db8bb4305dac1d15ed22272e68
ruiwng <ruiwng@zju.edu.cn>
2015-05-21 16:37:02 +0800

    D进制的A+B

--- PAT/basic/1022.cpp
+++ PAT/basic/1022.cpp
@@ -1,26 +1,21 @@
 #include  <stdio.h>
-#include  <stdlib.h>
 #include  <stack>
 using namespace std;
 int main()
 {
-	unsigned long long a,b;
+	int a,b;
 	int c;
 	stack<int> intStack;
-	while(scanf("%lld %lld %d",&a,&b,&c)!=EOF)
+	while(scanf("%d %d %d",&a,&b,&c)!=EOF)
 	{
 		while(!intStack.empty())
-		{
 			intStack.pop();
-		}
-		unsigned long long m=a+b;
+		int m=a+b;
 		do
 		{
 			intStack.push(m%c);
 			m/=c;
-		}while(m>=c);
-		if(m!=0)
-		    intStack.push(m);
+		}while(m!=0);
 		while(!intStack.empty())
 		{
 			printf("%d",intStack.top());
