c24720ebfd91e3d9ce5cdd331b32b8e6298663d0
ruiwng <ruiwng@zju.edu.cn>
2015-05-22 19:19:25 +0800

    德才论

--- PAT/basic/1015.cpp
+++ PAT/basic/1015.cpp
@@ -4,21 +4,21 @@
 #include <algorithm>
 using namespace std;
 
-typedef struct
+struct student
 {
      int num;
+     int sum;
      int score1;
      int score2;
-}student;
-
-bool compare(student stu1,student stu2)
-{
-     if((stu1.score1+stu1.score2)!=(stu2.score1+stu2.score2))
-           return (stu1.score1+stu1.score2)>(stu2.score1+stu2.score2);
-     if(stu1.score1!=stu2.score1)
-           return stu1.score1>stu2.score1;
-     return stu1.num<stu2.num;
-}
+     bool operator<(const student &s) const
+    {
+	if(this->sum!=s.sum)
+		return this->sum>s.sum;
+	if(this->score1!=s.score1)
+		return this->score1>s.score1;
+	return this->num<s.num;
+    }
+};
 
 int main()
 {
@@ -30,6 +30,7 @@ int main()
     {
           student stu;
           scanf("%d %d %d",&stu.num,&stu.score1,&stu.score2); 
+          stu.sum=stu.score1+stu.score2;
           if(stu.score1<low||stu.score2<low)
           {
              cnt--;
@@ -44,17 +45,15 @@ int main()
           else
               numVec[3].push_back(stu);    
      }
-     for(int i=0;i<4;i++)
+     for(int i=0;i<4;++i)
      {
-       sort(numVec[i].begin(),numVec[i].end(),compare);
+       sort(numVec[i].begin(),numVec[i].end());
      }
      printf("%d\n",cnt);
-     for(int i=0;i<4;i++)
+     for(int i=0;i<4;++i)
      {
-          for(vector<student>::const_iterator it=numVec[i].begin();it<numVec[i].end();++it)
-          {
-                printf("%d %d %d\n",it->num,it->score1,it->score2);
-           }
+          for(int j=0;j<numVec[i].size();++j)
+                printf("%d %d %d\n",numVec[i][j].num,numVec[i][j].score1,numVec[i][j].score2);
      }
      return 0;
 }
